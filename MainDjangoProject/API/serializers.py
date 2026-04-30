632436ab341ac521951ecc9273dbd5e89802e2fc
Meshref21 <mm333242@gmail.com>
2026-04-30 23:54:37 +0300

    transaction api endpoint + render html page

--- MainDjangoProject/API/serializers.py
+++ MainDjangoProject/API/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import BlogPost
+from addTransactions.models import Transaction
 
 class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
         fields = ['id', 'title', 'content', 'published_date']
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ['id', 'isIncome', 'amount', 'category', 'paymentMethod', 'description']
+
--- MainDjangoProject/API/views.py
+++ MainDjangoProject/API/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import BlogPost
-from .serializers import BlogPostSerializer
+from .serializers import BlogPostSerializer, TransactionSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from addTransactions.models import Transaction
 
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+class TransactionListCreate(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
