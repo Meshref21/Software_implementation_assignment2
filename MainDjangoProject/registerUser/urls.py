8761b38ee759e571f2fef2b65aaca82fd89883b0
master00x <xmanxx404@gmail.com>
2026-05-04 00:26:58 +0300

    fixed some issues in login & register pages

--- MainDjangoProject/registerUser/urls.py
+++ MainDjangoProject/registerUser/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.addRegisterPage, name='add_register_page'),
+    path('', views.addRegisterPage, name='register'),
 ]
