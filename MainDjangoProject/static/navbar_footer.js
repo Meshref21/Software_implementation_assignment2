57a32fbf0e7b0d6e8018a41ad7508709a7ab5104
Meshref21 <mm333242@gmail.com>
2026-05-06 14:56:10 +0300

    navbar + footer included in each html file

--- MainDjangoProject/static/navbar_footer.js
+++ MainDjangoProject/static/navbar_footer.js
@@ -17,3 +17,18 @@ document.getElementById("navbar").innerHTML = `
     </ul>
 </nav>
 `;
+
+document.getElementById("footer").innerHTML = `
+    <footer class="footer">
+      <div class="footer-content">
+        <p>&copy; 2026 Personal Budgeter. All rights reserved.</p>
+
+        <ul class="footer-links">
+          <li><a href="/home">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </div>
+    </footer>
+  `;
